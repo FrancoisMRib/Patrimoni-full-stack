c3cf4ce5625c96ec809ba695dceb57c4ecafc23a
FrancoisMRib <fr.ribere@gmail.com>
2024-06-18 10:46:34 +0200

    Mise à jour du quiz

---
+++ fil-rouge/src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question : "Laquelle de ces églises n'est pas à Toulouse ?",
+        option1 : "église Saint-Nicolas",
+        option2 : "cathédrale Saint-Etienne",
+        option3 : "cathédrale Notre-Dame",
+        option4 : "basilique Saint-Sernin",
+        ans : 3,
+    },
+    {
+        question : "Quel est le plus vieux pont de Toulouse ?",
+        option1 : "le pont d'Avignon",
+        option2 : "le pont Saint-Pierre",
+        option3 : "le pont Neuf",
+        option4 : "le pont Vieux",
+        ans : 3,
+    },
+    {
+        question : "Laquelle de ces églises n'est pas à Toulouse ?",
+        option1 : "église Saint-Nicolas",
+        option2 : "cathédrale Saint-Etienne",
+        option3 : "cathédrale Notre-Dame",
+        option4 : "basilique Saint-Sernin",
+        ans : 2,
+    },
+    {
+        question : "Quel lieu abrite la mairie de Toulouse ?",
+        option1 : "le Capitole",
+        option2 : "l'hôtel de Lafage",
+        option3 : "l'hôtel d'Assézat",
+        option4 : "chez Tonton",
+        ans : 1,
+    },
+    {
+        question : "Quel était le véritable nom de Saint-Sernin ?",
+        option1 : "Cerninin",
+        option2 : "Saturnin",
+        option3 : "Serlamin",
+        option4 : "Serninin",
+        ans : 2,
+    }
+]
